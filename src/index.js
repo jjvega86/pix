f242a6847f7627e015a38385ed2ac224f283368a
JJ <jj.vega86@gmail.com>
2021-02-25 20:18:35 -0500

    Refactored for separate app.js file

--- src/index.js
+++ src/index.js
@@ -1,12 +1,6 @@
-import React, { Component } from "react";
-import ReactDOM from "react-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/app';
 
-class App extends Component {
-  render() {
-    return <div>Hello World!</div>;
-  }
-}
-
-export default App;
 
 ReactDOM.render(<App />, document.querySelector("#root"));
