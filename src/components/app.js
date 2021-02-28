deddb54d9795b092fd1c3feb1d2c41559ec0974d
JJ <jj.vega86@gmail.com>
2021-02-28 16:33:37 -0500

    Refactored SearchBar onSubmit function as an arrow function; refactored App component into a class component

--- src/components/app.js
+++ src/components/app.js
@@ -1,15 +1,22 @@
-import React from "react";
+import React, {Component} from "react";
 import SearchBar from "./SearchBar";
 
-const App = (props) => {
-  const containerStyle = {
-    marginTop: "10px",
-  };
-  return (
-    <div className="ui container" style={containerStyle}>
-      <SearchBar />
-    </div>
-  );
-};
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    const containerStyle = {
+      marginTop: "10px",
+    };
+    return (
+      <div className="ui container" style={containerStyle}>
+        <SearchBar />
+      </div>
+    );
+  }
+}
 
 export default App;
+
