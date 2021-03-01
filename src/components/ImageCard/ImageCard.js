b817a22f7472fff6719fd98b1dd5dca0ae677c63
JJ <jj.vega86@gmail.com>
2021-03-01 08:28:46 -0500

    Imported ImageCard to ImageList, refactored ImageList to pass appropriate props to ImageCard

--- src/components/ImageCard/ImageCard.js
+++ src/components/ImageCard/ImageCard.js
@@ -1,15 +1,20 @@
-import React from 'react';
+import React, {Component} from "react";
 
 class ImageCard extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {  }
-    }
-    render() { 
-        return ( <div>
-            <img />
-        </div> );
-    }
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return (
+      <div>
+        <img
+          alt={this.props.image.description}
+          src={this.props.image.urls.regular}
+        />
+      </div>
+    );
+  }
 }
- 
+
 export default ImageCard;
