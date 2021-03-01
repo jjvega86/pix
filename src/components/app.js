5356f7538c1603546a64e30ba58a1a7b9052ef37
JJ <jj.vega86@gmail.com>
2021-02-28 20:39:20 -0500

    Added ImageList component to project with boilerplate

--- src/components/app.js
+++ src/components/app.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import unsplash from '../api/unsplash';
 import SearchBar from "./SearchBar";
+import ImageList from './ImageList';
 
 class App extends Component {
   constructor(props) {
@@ -22,6 +23,7 @@ class App extends Component {
     return (
       <div className="ui container" style={containerStyle}>
         <SearchBar onSubmit={this.onSearchSubmit} />
+        <ImageList />
         Found: {this.state.images.length} images
       </div>
     );
